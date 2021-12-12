dafe99cdd40de49eff004ce8934b67aa00b179de
wentie song <ellefry@live.cn>
2021-12-12 23:10:44 +0800

    add dbcontext

---
+++ RFC.Connector.Web/DataStorage/SapConnectorContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFC.Connector.Web.DataStorage
+{
+    public class SapConnectorContext : DbContext
+    {
+    }
+}
