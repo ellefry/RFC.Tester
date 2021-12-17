4e81482808ab116d28bd3f3a266db70fe7a961c3
wentie song <ellefry@live.cn>
2021-12-17 13:26:06 +0800

    add finishpart appservice

---
+++ BHSW2_2.Pinion.DataService/AppServices/SapRequestAppService.cs
@@ -0,0 +1,31 @@
+﻿using BHSW2_2.Pinion.DataService.AppServices.Interfaces;
+using BHSW2_2.Pinion.DataService.Clients.Abstracts;
+using BHSW2_2.Pinion.DataService.Clients.Dtos;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace BHSW2_2.Pinion.DataService.AppServices
+{
+    public class SapRequestAppService : ISapRequestAppService
+    {
+        private readonly SapConnectorContext _sapConnectorContext;
+
+        public SapRequestAppService(SapConnectorContext sapConnectorContext)
+        {
+            _sapConnectorContext = sapConnectorContext;
+        }
+
+        public async Task FinishPartAsync(FinishPartInput input)
+        {
+            var sapRequest = new SapRequest
+            {
+                Id = Guid.NewGuid(),
+                Content = JsonConvert.SerializeObject(input),
+                FunctionName = "FinishPartSapServiceHandler",
+            };
+            _sapConnectorContext.SapRequests.Add(sapRequest);
+            await _sapConnectorContext.SaveChangesAsync();
+        }
+    }
+}
--- BHSW2_2.Pinion.DataService/Domains/SapRequest.cs
+++ BHSW2_2.Pinion.DataService/Domains/SapRequest.cs
@@ -8,11 +8,11 @@ namespace BHSW2_2.Pinion.DataService
         [Key]
         public Guid Id { get; set; }
         public string FunctionName { get; set; }
-        public SapRequestStatus SapRequestStatus { get; set; } = SapRequestStatus.Scheduled
+        public SapRequestStatus SapRequestStatus { get; set; } = SapRequestStatus.Scheduled;
         public string Content { get; set; }
         public int Retries { get; set; }
         public string Error { get; set; }
-        public DateTimeOffset? Created { get; set; }
+        public DateTimeOffset? Created { get; set; } = DateTimeOffset.Now;
         public DateTimeOffset? Modified { get; set; }
 
         public void UpdateRetry(string error)
