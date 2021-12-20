03ff760fc570a21dccd6382292820282fc5d1c8c
ellefry <ellefry@qq.com>
2021-12-20 22:21:24 +0800

    add scrap part

---
+++ BHSW2_2.Pinion.DataService/Clients/Dtos/ScrapPartInput.cs
@@ -0,0 +1,17 @@
+﻿namespace BHSW2_2.Pinion.DataService.Clients.Dtos
+{
+    public class ScrapPartInput
+    {
+        public string ScrapNumber { get; set; }
+        public string ParentMaterial { get; set; }
+        /// <summary>
+        /// 报废工位号
+        /// </summary>
+        public string Vornr { get; set; }
+        public string Plant { get; set; }
+        public string StorageLocation { get; set; }
+        public string Material { get; set; }
+        public int Quantity { get; set; }
+        public int ScrapReason { get; set; }
+    }
+}
--- BHSW2_2.Pinion.DataService/Clients/SapConnectorClient.cs
+++ BHSW2_2.Pinion.DataService/Clients/SapConnectorClient.cs
@@ -57,7 +57,7 @@ namespace BHSW2_2.Pinion.DataService.Clients
                 {
                     StructureName = "EX_RETURN",
                     Data  = new List<RfcStructureData> {
-                        new RfcStructureData{ Key = "MSG_TYP", TargetValue = "S" },
+                        new RfcStructureData{ Key = "MSG_TYP", TargetValue = "/S" },
                         new RfcStructureData{ Key = "WERKS"},
                         new RfcStructureData{ Key = "MATNR" },
                         new RfcStructureData{ Key = "MAKTX" },
@@ -82,14 +82,100 @@ namespace BHSW2_2.Pinion.DataService.Clients
             var returnValue = await response.Content.ReadAsStringAsync();
             var finishPart = JsonConvert.DeserializeObject<ProcessRequestInput>(returnValue);
             var msgType = finishPart.ReturnStructure.Data.FirstOrDefault(r => r.Key == "MSG_TYP");
-            if (msgType.Value?.ToString() == msgType.TargetValue.ToString())
+            if (!msgType.TargetValue.ToString().Contains($"/{msgType.Value?.ToString()}"))
             {
                 var errorMessage = $"Sap returns error : MSG_TXT1 : {finishPart.ReturnStructure.Data.FirstOrDefault(r => r.Key == "MSG_TXT1")?.Value} " +
-                    $"MSG_TXT2: {finishPart.ReturnStructure.Data.FirstOrDefault(r => r.Key == "MSG_TX2")?.Value} " +
-                    $": MSG_TXT3 :{finishPart.ReturnStructure.Data.FirstOrDefault(r => r.Key == "MSG_TXT3")?.Value}";
+                    $" MSG_TXT2: {finishPart.ReturnStructure.Data.FirstOrDefault(r => r.Key == "MSG_TX2")?.Value} " +
+                    $" MSG_TXT3 :{finishPart.ReturnStructure.Data.FirstOrDefault(r => r.Key == "MSG_TXT3")?.Value}";
                 throw new Exception(errorMessage.ToString());
             }
 
+        }
+
+        public async Task ScrapPart(ScrapPartInput input)
+        {
+            var httpClient = _httpClientFactory.CreateClient(SapConnectorConstants.SapConnectorName);
+            ProcessRequestInput processRequestInput = new ProcessRequestInput
+            {
+                DestinationName = "mySAPdestination",
+                RfcFunctionName = "ZMM_DEAL_SCRAP1",
+                FunctionParam = new RfcParameter { 
+                    Data = new List<RfcStructureData> { 
+                        new RfcStructureData{Key = "I_REF_DOC_NO", Value=input.ScrapNumber },
+                        new RfcStructureData{Key = "I_PSTNG_DATE", Value= DateTimeOffset.Now.ToString("yyyyMMdd") },
+                        new RfcStructureData{Key = "I_DOC_DATE", Value=DateTimeOffset.Now.ToString("yyyyMMdd")},
+                        new RfcStructureData{Key = "I_MATERIAL", Value=input.ParentMaterial },
+                        new RfcStructureData{Key = "I_VORNR", Value=input.Vornr },
+                    }
+                },    
+                TableParams  = new RfcParameter
+                {
+                    StructureName = "TS_ITEM",
+                    Data  = new List<RfcStructureData> {
+                        new RfcStructureData{ Key = "MATERIAL", Value=input.Material },
+                        new RfcStructureData{ Key = "PLANT", Value=input.Plant },
+                        new RfcStructureData{ Key = "STGE_LOC", Value=input.StorageLocation },
+                        new RfcStructureData{ Key = "MOVE_TYPE", Value="Z01" },
+                        new RfcStructureData{ Key = "ENTRY_QNT", Value=input.Quantity },
+                        new RfcStructureData{ Key = "ZSKUNO", Value= "" },
+                        new RfcStructureData{ Key = "MOVE_REAS", Value="" },
+                    }
+                },
+
+                ReturnHeaders = new RfcParameter
+                {
+                    StructureName = "EX_HEADRET",
+                    Data  = new List<RfcStructureData> {
+                        new RfcStructureData{ Key = "MAT_DOC" },
+                        new RfcStructureData{ Key = "DOC_YEAR"},
+                        new RfcStructureData{ Key = "DOC_YEAR"},
+                        new RfcStructureData{ Key = "PSTNG_DATE" },
+                        new RfcStructureData{ Key = "DOC_DATE" },
+                        new RfcStructureData{ Key = "PR_UNAME" },
+                    }
+                },
+
+                ReturnStructure = new RfcParameter
+                {
+                    StructureName = "RETURN",
+                    Data  = new List<RfcStructureData> {
+                        new RfcStructureData{ Key = "TYPE", TargetValue = "/S" },
+                        new RfcStructureData{ Key = "ID"},
+                        new RfcStructureData{ Key = "NUMBER" },
+                        new RfcStructureData{ Key = "MESSAGE" },
+                        new RfcStructureData{ Key = "LOG_NO" },
+                        new RfcStructureData{ Key = "LOG_MSG_NO" },
+                        new RfcStructureData{ Key = "MESSAGE_V1" },
+                        new RfcStructureData{ Key = "MESSAGE_V2" },
+                        new RfcStructureData{ Key = "MESSAGE_V3" },
+                        new RfcStructureData{ Key = "MESSAGE_V4" },
+                        new RfcStructureData{ Key = "PARAMETER" },
+                        new RfcStructureData{ Key = "ROW" },
+                        new RfcStructureData{ Key = "FIELD" },
+                        new RfcStructureData{ Key = "SYSTEM" },
+
+                    }
+                },
+            };
+            var content = new StringContent(JsonConvert.SerializeObject(processRequestInput));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await httpClient.PostAsync($"{httpClient.BaseAddress}/sap", content);
+
+            await HandleException(response);
+
+            //process return
+            var returnValue = await response.Content.ReadAsStringAsync();
+            var scrapPart = JsonConvert.DeserializeObject<ProcessRequestInput>(returnValue);
+            var msgType = scrapPart.ReturnStructure.Data.FirstOrDefault(r => r.Key == "TYPE");
+            if (!msgType.TargetValue.ToString().Contains($"/{msgType.Value?.ToString()}"))
+            {
+                var errorMessage = $"Sap returns error : MESSAGE_V1 : {scrapPart.ReturnStructure.Data.FirstOrDefault(r => r.Key == "MESSAGE_V1")?.Value} " +
+                    $" MESSAGE_V2: {scrapPart.ReturnStructure.Data.FirstOrDefault(r => r.Key == "MESSAGE_V2")?.Value} " +
+                    $" MESSAGE_V3 :{scrapPart.ReturnStructure.Data.FirstOrDefault(r => r.Key == "MESSAGE_V3")?.Value}" +
+                    $" MESSAGE_V4 :{scrapPart.ReturnStructure.Data.FirstOrDefault(r => r.Key == "MESSAGE_V4")?.Value}";
+                throw new Exception(errorMessage.ToString());
+            }
         }
 
         private async Task HandleException(HttpResponseMessage response)
