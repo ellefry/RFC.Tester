3e00ee64136f93818d9b1099ee4b9553f10f8725
wentie song <ellefry@live.cn>
2021-12-27 10:03:35 +0800

    add MetaController

---
+++ BHSW2_2.Pinion.DataService/Controllers/MetaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace BHSW2_2.Pinion.DataService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MetaController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public MetaController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<dynamic> Get()
+        {
+            return await Task.FromResult(
+               new
+               {
+                   ApiVersion = "1.0.0",
+                   Name = _configuration.GetValue<string>("ServiceName") ?? "BHSW2_2.Pinion.Api",
+                   AssemblyVersion = this.GetType().Assembly.GetName().Version.ToString()
+               });
+        }
+    }
+}
