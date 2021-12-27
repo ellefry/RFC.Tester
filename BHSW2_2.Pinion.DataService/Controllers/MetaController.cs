3328b0f09d41a1eeb19f05629537dbf886968469
wentie song <ellefry@live.cn>
2021-12-27 11:41:12 +0800

    add line

--- BHSW2_2.Pinion.DataService/Controllers/MetaController.cs
+++ BHSW2_2.Pinion.DataService/Controllers/MetaController.cs
@@ -19,6 +19,8 @@ namespace BHSW2_2.Pinion.DataService.Controllers
         [HttpGet]
         public async Task<dynamic> Get()
         {
+            var s = string.Empty;
+            var s1 = string.Empty;
             return await Task.FromResult(
                new
                {
